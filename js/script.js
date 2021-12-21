14b43bb059afe1ed0192fd6d834a2d445a9a86f7
Thomricco <tommasoricco@hotmail.it>
2021-12-21 16:44:29 +0100

    fine

--- js/script.js
+++ js/script.js
@@ -11,32 +11,35 @@ function generaNumeri(minNumero, maxNumero) {
     return numbers;
 }
 
-let numbers = generaNumeri(1, 100)
-console.log(numbers)
-
-const outputHtml = document.querySelector('.numeri');
-
-outputHtml.innerHTML = `hai 30 secondi per memorizzare i numeri <br/> i numeri sono: ${numbers}`;
-
-setTimeout(off, 30000);
+function inserire() {
+    for (let i = 0; i < 5; i++) {
+        let numUtente = parseInt(prompt('inserisci uno dei cinque numeri'));
+        numberListUtente.push(numUtente);
+        if (numbers.includes(numUtente)) {
+            numEsatti.push(numUtente);
+        }
+    }
+}
 
 function off() {
     outputHtml.innerHTML = '';
 };
 
-
-const numberListUtente = [];
-
-setTimeout(inserire, 30000);
-
-function inserire() {
-    for (let i = 0; i < 5; i++) {
-        let numUtente = parseInt(prompt('inserisci uno dei cinque numeri'));
-        numberListUtente.push(numUtente);
-        
-    }
-
-    
+function mostraNumeriEsatti(){
+    outputHtml.innerHTML = `i tuoi numeri sono: ${numberListUtente}, quelli esatti:${numEsatti}`;
 }
 
+
+
+const numberListUtente = [];
+// contiene i numeri scelti dall'utente che fanno parte dei numeri esatti
+const numEsatti = [];
+let numbers = generaNumeri(1, 100);
+console.log(numbers)
+const outputHtml = document.querySelector('.numeri');
+outputHtml.innerHTML = `hai 30 secondi per memorizzare i numeri <br/> i numeri sono: ${numbers}`;
+setTimeout(off, 30000);
+setTimeout(inserire, 30000);
+setTimeout(mostraNumeriEsatti, 30000);
 console.log(numberListUtente);
+console.log(numEsatti);
